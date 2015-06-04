f78ebbc846e95220327b9e8aec575a5937f7a9a1
nwpointer <nwpointer@gmail.com>
2015-06-04 15:28:29 -0700

    Update unpack.v

--- unpack.v
+++ unpack.v
@@ -1,7 +1,7 @@
 // May 28 2015
 // Nathan Pointer
 
-module unpack(clk, rst, io, eta, phi, et, e);
+module unpack(clk, rst, io, eta, phi, et, e, eout, etout);
 	input clk;
 	input rst;
 	input eta; // 32
@@ -9,24 +9,26 @@ module unpack(clk, rst, io, eta, phi, et, e);
 	input et;   // 0-1000
 	input e;
 	input io;
+	output eout;
+	output etout;
 
 	reg[0:31] index;
 	integer i;
 
-	reg [4:0] etas [4:0]; // 32 blocks of 32
-	reg [4:0] phis [4:0]; // 32 blocks of 32
-	reg [10:0] ets [4:0];
-	reg [10:0] es [4:0];
+	reg [7:0] etas [1023:0]; // 32 blocks of 32
+	reg [7:0] phis [1023:0]; // 32 blocks of 32
+	reg [7:0] ets [1023:0];
+	reg [7:0] es [1023:0];
 
-	reg et_temp[10:0];
-	reg e_temp[10:0];
+	reg[7:0] et_temp;
+	reg[7:0] e_temp;
 
 	localparam gets = 0, puts = 1;
 
 	always @(posedge clk or posedge rst) begin
 		if (rst) begin
 			index <=0;
-			for (i=0; i<8; i=i+1) begin
+			for (i=0; i<1023; i=i+1) begin
 				etas[i] <= 0;
 				phis[i] <= 0;
 				ets[i] <= 0;
@@ -45,7 +47,7 @@ module unpack(clk, rst, io, eta, phi, et, e);
 					end
 				gets:
 					begin
-						i = eta + (phi*8);
+						i = eta + (phi*32);
 						et_temp = ets[i];
 						e_temp = es[i];	
 					end
@@ -54,8 +56,7 @@ module unpack(clk, rst, io, eta, phi, et, e);
 	end
 
 	// Outputs
-	assign et = et_temp;
-	assign e = e_temp;
+	assign etout = et_temp;
+	assign eout = e_temp;
 
 endmodule
-
